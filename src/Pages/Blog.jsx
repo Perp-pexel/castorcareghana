9eaf71bca9107eff93a55dcbc74ae64a1c354fca
Perp-pexel <perpaman44@gmail.com>
2025-06-23 04:18:15 +0000

    blog

--- src/Pages/Blog.jsx
+++ src/Pages/Blog.jsx
@@ -8,10 +8,13 @@ const Blog = () => {
   return (
     <div>
       <Nav />
-      <div className="flex flex-col justify-start items-start mt-[10%] ml-[60%] text-left">
-        <h1 className="text-4xl text-gray-800 font-bold">Castor EcoLearn Hub</h1>
-        <p className="text-gray-600 text-lg mt-2">Stay updated with our latest news, stories, and insights.</p>
-      </div>
+      <div className="flex flex-col  text-right md:text-right mt-[25%] mb-10 md:mb-[0] md:mt-[12%] px-4 md:px-0">
+  <h1 className="text-3xl md:text-4xl text-gray-800 font-bold mr-20">Castor EcoLearn Hub</h1>
+  <p className="text-gray-600 text-base md:text-lg mt-2 mr-10">
+    Stay updated with our latest news, stories, and insights.
+  </p>
+</div>
+
       <News />
       <Benefit />
       <Media />
